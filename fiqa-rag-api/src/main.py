4d09ece992cbe1a32af21f828f185ee18f2d9d34
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2026-09-20 03:42:12 +0000

    feat: add the split of text in the main

--- fiqa-rag-api/src/main.py
+++ fiqa-rag-api/src/main.py
@@ -5,22 +5,25 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.repositories.source_dataset_repository import SourceDatasetRepository
+from src.processors.text_splitter_processor import TextSplitterProcessor
 
 def main() -> None:
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    
     repository = SourceDatasetRepository()
+    splitter = TextSplitterProcessor()
+    
     try:
         raw_documents = repository.get_raw_documents()
         print(f"Documents: {len(raw_documents)}")
-        first_doc_id = list(raw_documents.keys())[0]
-        print(f"Sample Doc [{first_doc_id}]: {raw_documents[first_doc_id]}\n")
-        queries = repository.get_queries()
-        print(f"Queries: {len(queries)}")
-        first_query_id = list(queries.keys())[0]
-        print(f"Sample Query [{first_query_id}]: {queries[first_query_id]}\n")
-        test_mapping = repository.get_test_mapping()
-        print(f"Mappings: {len(test_mapping)}")
-        print(f"Sample Mapping: {test_mapping[0]}\n")
+        doc_id = list(raw_documents.keys())[1]
+        target_doc = raw_documents[doc_id]
+        print(f"Original Doc [{doc_id}]: {target_doc}\n")
+        chunks = splitter.split_text(document_id=doc_id, title=target_doc.get("title", ""), text=target_doc.get("text", ""))
+        print(f"Total Chunks Generated: {len(chunks)}")
+        for chunk in chunks:
+            print(f"--> Chunk ID: {chunk['chunk_id']}")
+            print(f"    Text: {chunk['texto']}\n")
         
     except Exception as error:
         print(f"Error: {error}")
